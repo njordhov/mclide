3650a823327f636ef343281e397e29e10a8e6006
Terje Norderhaug <terje@in-progress.com>
2013-05-21 14:53:55 -0700

    ccl init improved

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 # Run to build MCLIDE
 
 CCL_DEFAULT_DIRECTORY=./ccl
-export CCL_DEFAULT_DIRECTORY
-./ccl/dx86cl64 --batch --no-init --load "./mclide/build.lisp"
+# export CCL_DEFAULT_DIRECTORY
+./ccl/dx86cl64 --batch --no-init --load "./mclide/build.lisp" --eval "(ccl::quit)"
